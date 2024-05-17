bfeb6ebf7145d43056d3e830978c6f6f98c693a1
JimmyMP <brayan.cuyate.m@uni.pe>
2024-05-17 06:53:32 -0500

    <FE>

--- JavaScript/src/contract.ts
+++ JavaScript/src/contract.ts
@@ -1,25 +1,22 @@
 import { NearBindgen, NearPromise, UnorderedMap, near, call, assert, view } from 'near-sdk-js';
 import { ONE_NEAR } from 'near-sdk-js/lib/types';
-//npm run build
-//npm run deploy
 
-
-//Creamos una clase llamada participante
+//Creamos una clase llamada proyecto
 @NearBindgen({})
-class Participante {
+class Proyecto {
   nombre: string;
   cuenta: string;
-  edad: number;
-  certificado: boolean;
-  correo: string;
+  descripcion: string;
+  monto_total: number;
+  completado: boolean;
 
   //Inicializamos el objeto
-  constructor(nombre: string, cuenta: string, edad: number) {
+  constructor(nombre: string, cuenta: string, descripcion:string, monto_total: number) {
     this.nombre = nombre;
     this.cuenta = cuenta;
-    this.edad = edad;
-    this.certificado = false;
-    this.correo = ''
+    this.descripcion = descripcion;
+    this.monto_total = monto_total;
+    this.completado = false;
   }
 }
 
@@ -27,140 +24,142 @@ class Participante {
 @NearBindgen({})
 class StarterContract {
   //Creamos una colección para almacenar información en nuestro contrato.
-  participantes: UnorderedMap<Participante> = new UnorderedMap<Participante>('p');
-  // Método de ESCRITURA para eliminar un participante
-  //near call jimmymp.testnet eliminar_participante '{"cuenta":"jimmymp.testnet"}' --accountId jimmymp.testnet
-@call({})
-eliminar_participante({ cuenta }: { cuenta: string }): boolean {
-
-    // Si la cuenta ejecutando el comando no es aklassen.testnet, no podrá hacerse ningún cambio.
-    assert(near.signerAccountId() == "jimmymp.testnet", "No tienes permisos para ejecutar este comando.");
-
-    // Buscamos al participante
-    let participante = this.participantes.get(cuenta);
-
-    // Verificamos si encontramos al participante y si no está certificado
-    if (participante && !participante.certificado) {
-        // Eliminamos al participante de la lista
-        this.participantes.remove(cuenta);
-        near.log("Participante eliminado exitosamente.");
-        return true;
-    } else {
-        // Si el participante no existe o está certificado, regresamos false
-        near.log("No se puede eliminar al participante.");
-        return false;
-    }
-}
-
+  proyectos: UnorderedMap<Proyecto> = new UnorderedMap<Proyecto>('p');
 
   /**
-   * Método de ESCRITURA para registrar un nuevo participante
+   * Método de ESCRITURA para registrar un nuevo proyecto
    * El comando para utilizarlo en la terminal es:
-   *  >> near call $CONTRATO set_participante '{"nombre":"NOMBRE","edad":18}' --accountId cuenta.near --amount 1
+   *  >> near call $CONTRATO set_proyecto '{"nombre":"Proyecto","descripcion":"DescripcionProyecto","monto_total":150}' --accountId example.testnet --amount 1.1
    *    * $CONTRATO es una variable de entorno que contiene el id de la cuenta del contrato
    * 
-   * @param nombre string que requiere el nombre del participante a registrar
-   * @param edad entero de 32 bits sin signo que requiere la edad del participante
+   * @param nombre string que requiere el nombre del proyecto
+   * @param descripcion string que requiere la descripción del proyecto
+   * @param monto_total entero de 32 bits sin signo que requiere el monto total que requiere el proyecto
    * 
    * Es necesario enviarle 1 NEAR (o más) como pago a este método.
    * Como vamos a aceptar pagos con este método, lo marcamos como payableFunction.
-   * El método registra la cuenta que firma la tx cómo la cuenta del participante registrándose.
+   * El método registra la cuenta que firma la tx cómo la cuenta del proyecto registrándose.
    */
-  //near call jimmymp.testnet set_participante '{"nombre":"Isaac","edad":28}' --accountId jimmymp.testnet --amount 1.1 
 
   @call({ payableFunction: true })
-  set_participante({ nombre, edad }: { nombre: string, edad: number }): void {
+  set_proyecto({ nombre, descripcion, monto_total }: { nombre: string, descripcion:string, monto_total: number }): void {
+    // Si la cuenta ejecutando el comando no es jimmymp1379.testnet, no podrá hacerse ningún cambio.
+    assert(near.signerAccountId() == "jimmymp1379.testnet", "No tienes permisos para ejecutar este comando.");
+
     //Usamos el objeto near para obtener datos de la transacción.
     const cuenta = near.signerAccountId();
     const deposito = near.attachedDeposit();
 
     //Hacemos validaciones. Queremos que:
-    //* No pongan 0 como edad, osea que la edad sea mayor a 0.
-    //* El nombre tenga más de 3 caractéres.
-    //* Paguen 1 NEAR cada que se registren
-    assert(edad > 0, "Edad inválida.");
+    //* No pongan 0 como monto_total, osea que el monto_total sea mayor a 0.
+    //* El nombre y descripcion tenga más de 3 caractéres.
+    //* Paguen 1 NEAR cada que se registren en el contrato.
+    assert(monto_total > 0, "Monto inválido.");
     assert(nombre.length >= 3, "El nombre debe contener 3 o más caractéres.");
+    assert(descripcion.length >= 3, "La descripción debe contener 3 o más caractéres.");
     assert(deposito >= ONE_NEAR, "Debes de pagar 1 NEAR para registrarte.");
 
     //Instanciamos la clase (creamos un objeto) y le mandamos los datos al constructor.
-    const participante = new Participante(nombre, cuenta, edad);
+    const proyecto = new Proyecto(nombre, cuenta, descripcion, monto_total);
     
     //Guardamos la información en la blockchain.
     //UnorderedMap requiere una clave y el dato a guardar.
     //Dado a que se requiere una clave única, vamos a usar la cuenta como clave.
     //Para más información consulta: https://docs.near.org/develop/contracts/basics#sdk-collections
-    this.participantes.set(cuenta, participante);
+    this.proyectos.set(cuenta, proyecto);
 
     //Le enviamos un mensaje de confirmación a la consola.
-    near.log("Registro creado exitosamente.");
+    near.log("Proyecto creado exitosamente.");
   }
 
   /**
-   * Método de LECTURA que regresa un participante
+   * Método de LECTURA que regresa un proyecto
    * El comando para utilizarlo en la terminal es:
-   *  >> near view $CONTRATO get_participante '{"cuenta":"CUENTA.NEAR"}'
+   *  >> near view $CONTRATO get_proyecto '{"cuenta":"CUENTA.NEAR"}'
    * @param cuenta string que contiene la cuenta (key) del usuario a consultar
    */
-  //near view jimmymp.testnet get_participante '{"cuenta":"jimmymp.testnet"}'
+
   @view({})
-  get_participante({ cuenta }: { cuenta: string }) {
-    return this.participantes.get(cuenta);
+  get_proyecto({ cuenta }: { cuenta: string }) {
+    return this.proyectos.get(cuenta);
   }
 
   /**
-   * Método de LECTURA que regresa toda la lista de participantes registrados
+   * Método de LECTURA que regresa toda la lista de los proyectos registrados
    * El comando para utilizarlo en la terminal es:
-   *  >> near view $CONTRATO get_participantes '{}'
+   *  >> near view $CONTRATO get_proyectos '{}'
    */
-  //near view jimmymp.testnet get_participantes
+
   @view({})
-  get_participantes() {
-    return this.participantes.toArray();
+  get_proyectos() {
+    return this.proyectos.toArray();
   }
 
   /**
-   * Método de ESCRITURA para certificar a un participante
-   * Además, transfiere 5 NEAR como compensación al participante que se haya certificado.
+   * Método de ESCRITURA para marcar como completo un proyecto
    * El comando para utilizarlo en la terminal es:
-   *  >> near call $CONTRATO set_certificado '{"cuenta":"cuenta.near"}' --accountId cuenta.near --amount 1
+   *  >> near call $CONTRATO set_completado '{"cuenta":"cuenta.near"}' --accountId cuenta.near --amount 1
    * 
-   * @param cuenta string que contiene la cuenta del participante a certificar
+   * @param cuenta string que contiene la cuenta del proyecto completado
    */
   @call({})
-  set_certificado({ cuenta }: { cuenta: string }) {
+  set_completado({ cuenta }: { cuenta: string }) {
 
-    //Si la cuenta ejecutando el comando no es aklassen.testnet, no podrá hacerse ningún cambio.
-    assert(near.signerAccountId() == "jimmymp.testnet", "No tienes permisos para ejecutar este comando.");
+    //Si la cuenta ejecutando el comando no es jimmymp1379.testnet, no podrá hacerse ningún cambio.
+    assert(near.signerAccountId() == "jimmymp1379.testnet", "No tienes permisos para ejecutar este comando.");
 
-    //Buscamos al participante. En este caso se declara como let porque vamos a modificarlo.
-    let participante = this.participantes.get(cuenta);
+    //Buscamos al proyecto. En este caso se declara como let porque vamos a modificarlo.
+    let proyecto = this.proyectos.get(cuenta);
 
-    //Necesitamos evaluar si la línea de arriba encontró al participante.
-    if (participante && participante.certificado == false) {
-      participante.certificado = true;
+    //Necesitamos evaluar si la línea de arriba encontró al proyecto
+    if (proyecto && proyecto.completado == false) {
+      proyecto.completado = true;
 
-      //Le transferimos al participante 5 NEAR como premio por haber logrado su certificación.
-      const promise = near.promiseBatchCreate(cuenta);
-      near.promiseBatchActionTransfer(promise, ONE_NEAR * BigInt(5));
-
-      //Y guardamos los cambios hechos al participante
-      this.participantes.set(cuenta, participante);
-      near.log("Participante certificado. El participante ha recibido su recompensa de 5 NEAR.");
+      //Y guardamos los cambios hechos al proyecto
+      this.proyectos.set(cuenta, proyecto);
+      near.log("Proyecto Completado");
 
       //Por último, regresamos true indicando que la acción se completó exitosamente.
       return true;
     }
-    else if (participante && participante.certificado == true) {
-      //Si no encuentra al participante, o si este ya está certificado
+    else if (proyecto && proyecto.completado == true) {
+      //Si no encuentra al proyecto, o si este ya está completado
       //regresamos false.
-      near.log("El participante ya se encontraba certificado.");
+      near.log("El proyecto ya se encontraba completado.");
 
       return false;
     }
     else {
-      near.log("Participante no encontrado.");
+      near.log("Proyecto no encontrado.");
 
       return false;
     }
+  }
+  // Método de ESCRITURA para eliminar un proyecto
+  /*
+  * El comando para utilizarlo en la terminal es:
+   *  >> near call $CONTRATO eliminar_proyecto'{"cuenta":"jimmymp.testnet"}' --accountId jimmymp.testnet
+   *    * $CONTRATO es una variable de entorno que contiene el id de la cuenta del contrato
+  */
+  @call({})
+  eliminar_proyecto({ cuenta }: { cuenta: string }): boolean {
+
+      // Si la cuenta ejecutando el comando no es jimmymp.testnet, no podrá hacerse ningún cambio.
+      assert(near.signerAccountId() == "jimmymp1379.testnet", "No tienes permisos para ejecutar este comando.");
+
+      // Buscamos al proyecto
+      let proyecto = this.proyectos.get(cuenta);
+
+      // Verificamos si encontramos al proyecto y si está completado
+      if (proyecto && proyecto.completado) {
+          // Eliminamos al proyecto de la lista
+          this.proyectos.remove(cuenta);
+          near.log("Proyecto eliminado exitosamente.");
+          return true;
+      } else {
+          // Si el proyecto no existe o no está completado, regresamos false
+          near.log("No se puede eliminar el proyecto.");
+          return false;
+      }
   }
 }
